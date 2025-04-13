82e8eea5f570a054bfe5fb01324ac1c4762297f8
ananyakumar05 <ananya_kumar@icloud.com>
2025-04-14 01:48:57 +0530

    Saved!

--- savedata.php
+++ savedata.php
@@ -1,16 +1,22 @@
 <?php
-$name=$_POST['fullname'];
-$number=$_POST['mobileno'];
-$email=$_POST['emailid'];
-$age=$_POST['age'];
-$gender=$_POST['gender'];
-$blood_group=$_POST['blood'];
-$address=$_POST['address'];
-$conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
-$sql= "INSERT INTO donor_details(donor_name,donor_number,donor_mail,donor_age,donor_gender,donor_blood,donor_address) values('{$name}','{$number}','{$email}','{$age}','{$gender}','{$blood_group}','{$address}')";
-$result=mysqli_query($conn,$sql) or die("query unsuccessful.");
+if (!isset($_POST['submit'])) {
+    die("Access denied.");
+}
 
-header("Location: http://localhost/BDMS/home.php");
+$name = $_POST['fullname'];
+$number = $_POST['mobileno'];
+$email = $_POST['emailid'];
+$age = $_POST['age'];
+$gender = $_POST['gender'];
+$blood_group = $_POST['blood'];
+$address = $_POST['address'];
 
+$conn = mysqli_connect("localhost", "root", "", "blood_donation") or die("Connection error");
+$sql = "INSERT INTO donor_details(donor_name, donor_number, donor_mail, donor_age, donor_gender, donor_blood, donor_address) 
+        VALUES ('{$name}', '{$number}', '{$email}', '{$age}', '{$gender}', '{$blood_group}', '{$address}')";
+$result = mysqli_query($conn, $sql) or die("Query unsuccessful: " . mysqli_error($conn));
+
+// Redirect to home.php (relative path)
+header("Location: home.php");
 mysqli_close($conn);
- ?>
+?>
